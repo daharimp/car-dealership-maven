fae80e81782197a68c3e3f4ba39ecc5afbe54717
daharimp <daharimorgan@gmail.com>
2026-05-14 14:11:25 -0700

    Inital Feature Branch commit

--- src/main/java/com/skills4it/dealership/ui/UserInterface.java
+++ src/main/java/com/skills4it/dealership/ui/UserInterface.java
@@ -41,7 +41,7 @@ public class UserInterface {
     private void displayHeader() {
         System.out.println();
         System.out.println("=============================================");
-        System.out.println(dealership.getName());
+        System.out.println("Welcome to the worst place ever: " + dealership.getName());
         System.out.println(dealership.getAddress() + " | " + dealership.getPhone());
         System.out.println("=============================================");
     }
