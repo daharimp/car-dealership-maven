09b884cc5b3efffebd5ee7631e99fc3fa5e7d3a1
daharimp <daharimorgan@gmail.com>
2026-05-14 14:11:57 -0700

    Menu change

--- src/main/java/com/skills4it/dealership/ui/UserInterface.java
+++ src/main/java/com/skills4it/dealership/ui/UserInterface.java
@@ -41,7 +41,7 @@ public class UserInterface {
     private void displayHeader() {
         System.out.println();
         System.out.println("=============================================");
-        System.out.println("Welcome to the worst place ever: " + dealership.getName());
+        System.out.println("Welcome to the worst place dealership ever! " + dealership.getName());
         System.out.println(dealership.getAddress() + " | " + dealership.getPhone());
         System.out.println("=============================================");
     }
